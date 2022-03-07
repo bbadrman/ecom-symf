4b74b09f276651646b57a6f759deac78fe457fca
bbadrman <bbadrman@gmail.com>
2022-03-07 20:39:06 +0100

    1. Comprendre l'utilité de la sessionInterface() de Symfony 5

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
+        $session->remove('cart');
+        $cart = $session->get('cart');
+        
         return $this->render('home/index.html.twig');
     }
 }
